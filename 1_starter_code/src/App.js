1305439ac8ed78a2e1ea7b75bd70d3d54740ae6d
codeclassifiers <codeclassifiers@gmail.com>
2020-10-08 07:34:44 +0530

    Solution added

---
+++ 1_starter_code/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Features from './components/Features';
+import Pricing from './components/Pricing';
+import Home from './components/Home';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ solution_1/src/components/Pricing.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Pricing() {
+    return(
+        <div>
+            <h1>Pricing</h1>
+        </div>
+    )
+}
+
+export default Pricing;
