46ff9ed527e48677d06ad891f63754e793a969b5
Emma Slight <emma.slight@icloud.com>
2020-10-16 22:54:19 +1300

    solution_4 setup pages

---
+++ solution_4/src/components/Blog.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Blog(props) {
+  return (
+    <div className='text-center'>
+      <h3 className='pt-5'>Blog</h3>
+    </div>
+  )
+}
+
+export default Blog
---
+++ solution_4/src/components/Login.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Login() {
+  return (
+    <div className='text-center'>
+      <h3 className='pt-5'>a login page</h3>
+    </div>
+  )
+}
+
+export default Login
